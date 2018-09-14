fe61cd7d2dd83b449ecf692663c7c1f8055c88a2
ioms <769737461@qq.com>
2018-09-14 17:55:58 +0800

    ~

---
+++ dubbo-consumer/src/main/java/cn/ioms/dubbo/entity/User.java
@@ -0,0 +1,55 @@
+package cn.ioms.dubbo.entity;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private Date createDate;
+
+    private Date modifyDate;
+
+    private String password;
+
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ dubbo-consumer/src/main/java/cn/ioms/dubbo/service/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.ioms.dubbo.service;
+
+import cn.ioms.dubbo.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
